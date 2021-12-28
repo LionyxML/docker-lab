81a14bba25148103dd60120c396e1e8f1f7b321a
LionyxML <rahul.juliato@gmail.com>
2021-12-28 12:41:43 -0300

    Basic docker container config and readme

--- docked-react-app/src/App.tsx
+++ docked-react-app/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This app is running inside a docker container!</p>
+        <p>This app is running inside a docker container!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
